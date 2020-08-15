0845ac8e57f7964fcb9bc97959cece720547c815
Mostain <bill.rassel@gmail.com>
2020-08-15 20:59:43 +0600

    mateors couchbase database version 1.00

---
+++ mcb.go
@@ -0,0 +1,472 @@
+package mcb
+
+import (
+	b64 "encoding/base64"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+	"reflect"
+	"strconv"
+	"strings"
+
+	"github.com/elliotchance/orderedmap"
+)
+
+type metrics struct {
+	ElapsedTime   string `json:"elapsedTime"`
+	ExecutionTime string `json:"executionTime"`
+	ResultCount   int    `json:"resultCount"`
+	ResultSize    int    `json:"resultSize"`
+	ErrorCount    int    `json:"errorCount"`
+}
+
+//Struct First character must be a capital letter
+type errorMsg struct {
+	Code    int    `json:"code"`
+	Message string `json:"msg"`
+}
+
+//ResponseMessage Main struct
+type ResponseMessage struct {
+	RequestID string `json:"requestID"`
+	//Result    []string   `json:"results"`
+	Result  []interface{} `json:"results"`
+	Errors  []errorMsg    `json:"errors"`
+	Status  string        `json:"status"`
+	Metrics metrics       `json:"metrics"`
+}
+
+type nqlQuery struct {
+	Statement string `json:"statement"`
+	Pretty    bool   `json:"pretty,omitempty"`
+	//Metrics   bool   `json:"metrics"`
+}
+
+//DB is a database handle
+type DB struct {
+	url      string
+	username string
+	password string
+}
+
+//Connect method
+func Connect(url, userName, passWord string) *DB {
+
+	db := &DB{url: url, username: userName, password: passWord}
+	return db
+}
+
+//Exec (Result, error)
+// func (db *DB) Exec(query string, args ...interface{}) {
+
+// }
+
+func (db *DB) base64UserPassword() (base64 string) {
+
+	plainTxt := fmt.Sprintf("%s:%s", db.username, db.password)
+	base64 = b64.StdEncoding.EncodeToString([]byte(plainTxt))
+
+	return
+
+}
+
+func (db *DB) authorization() (auth string) {
+
+	//"Basic QWRtaW5pc3RyYXRvcjpNb3N0YWluMzIxJA=="
+	auth = fmt.Sprintf("Basic %s", db.base64UserPassword())
+	return
+}
+
+//GetRows no parameter
+func (pres *ResponseMessage) GetRows() []map[string]interface{} {
+
+	rows := make([]map[string]interface{}, 0)
+
+	for _, v := range pres.Result {
+
+		ms := v.(map[string]interface{})
+		//fmt.Println(i, "==>", ms["master_erp"])
+		rows = append(rows, ms)
+
+	}
+
+	return rows
+
+}
+
+//GetBucketRows ...
+func (pres *ResponseMessage) GetBucketRows(bucketName string) []map[string]interface{} {
+
+	rows := make([]map[string]interface{}, 0)
+
+	for _, v := range pres.Result {
+
+		ms := v.(map[string]interface{})
+		//fmt.Println(i, "==>", ms["master_erp"])
+		if len(bucketName) > 1 {
+			rows = append(rows, ms[bucketName].(map[string]interface{}))
+		} else {
+			rows = append(rows, ms)
+		}
+
+	}
+
+	return rows
+
+}
+
+//Query ...Select
+func (db *DB) Query(sql string) *ResponseMessage {
+
+	url := db.url //"http://localhost:8093/query/service"
+	method := "POST"
+
+	jsonTxt := sqlStatementJSON(sql)
+	//payload := strings.NewReader("{\n \"statement\": \"SELECT * FROM master_erp WHERE type='login_session'\"\n}\n\n")
+
+	payload := strings.NewReader(jsonTxt)
+	// RETURNING *\"\n}\n\n
+
+	client := &http.Client{}
+	req, err := http.NewRequest(method, url, payload)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+	req.Header.Add("Content-Type", "application/json")
+	req.Header.Add("Authorization", db.authorization())
+	//req.Header.Add("Authorization", "Basic QWRtaW5pc3RyYXRvcjpNb3N0YWluMzIxJA==") //***
+	//QWRtaW5pc3RyYXRvcjpNb3N0YWluMzIxJA==
+
+	res, err := client.Do(req)
+	body, err := ioutil.ReadAll(res.Body)
+	defer res.Body.Close()
+
+	//local variable as a pointer
+	var resPonse ResponseMessage
+
+	json.Unmarshal(body, &resPonse)
+
+	return &resPonse
+}
+
+//queryRequest ...
+func (db *DB) queryRequest(jsonText string) *ResponseMessage {
+
+	url := db.url //"http://localhost:8093/query/service"
+	method := "POST"
+
+	//payload := strings.NewReader("{\n \"statement\": \"INSERT INTO master_erp (KEY, VALUE) VALUES (\\\"login_session::104\\\", { \\\"type\\\": \\\"login_session\\\", \\\"cid\\\":1,\\\"device_info\\\":\\\"device_log::2\\\",\\\"session_code\\\":\\\"000-1111-2222-333-4444\\\",\\\"login_id\\\":1,\\\"ip_address\\\":\\\"0.0.0.0\\\",\\\"city\\\":\\\"Dhaka\\\",\\\"country\\\":\\\"Bangladesh\\\",\\\"login_time\\\":\\\"2020-06-11 10:30:00\\\",\\\"create_date\\\":\\\"2020-06-11 09:00:30\\\",\\\"status\\\": 1 }) RETURNING *\"\n}\n\n")
+	payload := strings.NewReader(jsonText)
+
+	client := &http.Client{}
+	req, err := http.NewRequest(method, url, payload)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+	req.Header.Add("Content-Type", "application/json")
+	req.Header.Add("Authorization", db.authorization())
+	//req.Header.Add("Authorization", "Basic QWRtaW5pc3RyYXRvcjpNb3N0YWluMzIxJA==")
+	//QWRtaW5pc3RyYXRvcjpNb3N0YWluMzIxJA==
+
+	res, err := client.Do(req)
+	body, err := ioutil.ReadAll(res.Body)
+	defer res.Body.Close()
+
+	//local variable as a pointer
+	var resPonse ResponseMessage
+
+	json.Unmarshal(body, &resPonse)
+
+	//resPonse.Errors[0].Code
+	//resPonse.Status
+
+	// fmt.Println(string(body))
+	// fmt.Println("resPonse::>>", resPonse)
+	// bytes, _ := json.Marshal(resPonse)
+	// fmt.Println()
+	// fmt.Println()
+	// fmt.Println(string(bytes))
+
+	return &resPonse
+}
+
+//ProcessData ...
+//form url.Values | r *http.Request
+func (db *DB) ProcessData(form url.Values, intrfc interface{}) []byte {
+
+	//r.ParseForm()
+	//r.Form
+
+	oMap := prepareData(form, intrfc)
+	//fmt.Println("oMap:>", oMap)
+
+	//fmt.Println("intrfc:::::", intrfc)
+	mpRes := make(map[string]interface{}, 0)
+
+	// Iterate through all elements from oldest to newest:
+	for el := oMap.Front(); el != nil; el = el.Next() {
+
+		//fmt.Println(el.Key, "===", el.Value)
+		//fmt.Printf("%v %T\n", el.Value, el.Value)
+		if el.Value == nil || el.Value == "" {
+			//fmt.Println("nil value for", el.Key)
+			//mpRes[el.Key.(string)] = nil
+
+		} else {
+			mpRes[el.Key.(string)] = el.Value
+		}
+
+	}
+
+	bytes, _ := json.Marshal(mpRes)
+	//fmt.Println("bytes:", string(bytes))
+
+	//fmt.Println()
+	//var logSessData2 models.LoginSession
+	json.Unmarshal(bytes, intrfc) //***s
+	//bytes2, _ := json.Marshal(intrfc)
+
+	return bytes
+
+}
+
+//EncodeBase64 usually used for Basic Authorization in http header.
+//format= username:password
+//func EncodeBase64(plainText string) (base64 string) {
+
+//import b64 "encoding/base64"
+//data := "Administrator:Mostain321$"
+//base64 = b64.StdEncoding.EncodeToString([]byte(plainText))
+//fmt.Println(base64)
+//return
+//}
+
+//UpsertIntoBucket ...
+func (db *DB) UpsertIntoBucket(docID, bucketName string, intrfc interface{}) *ResponseMessage {
+
+	bytes, _ := json.Marshal(intrfc)
+
+	//upsertQueryBuilder()
+	upsertQuery := upsertQueryBuilder(bucketName, docID, string(bytes))
+
+	nqlInsertStatement := sqlStatementJSON(upsertQuery)
+
+	responseMessage := db.queryRequest(nqlInsertStatement)
+
+	return responseMessage
+
+}
+
+//InsertIntoBucket ...
+func (db *DB) InsertIntoBucket(docID, bucketName string, intrfc interface{}) *ResponseMessage {
+
+	bytes, _ := json.Marshal(intrfc)
+
+	//upsertQueryBuilder()
+	insertQuery := insertQueryBuilder(bucketName, docID, string(bytes))
+
+	nqlInsertStatement := sqlStatementJSON(insertQuery)
+
+	responseMessage := db.queryRequest(nqlInsertStatement)
+
+	return responseMessage
+
+}
+
+//Insert method for insert
+func (db *DB) Insert(form url.Values, intrfc interface{}) *ResponseMessage {
+
+	//bucketName := "master_erp"
+	//docID := "12121"
+	bytes := db.ProcessData(form, intrfc)
+	//db.ProcessData(form, intrfc)
+	//json.Unmarshal(bytes, intrfc) //***s
+
+	//fmt.Println("DATA>>>>", intrfc)
+
+	//bytes, _ := json.Marshal(intrfc)
+	//fmt.Println("intrfcBytes:", string(bytes))
+
+	docID := form.Get("aid") //docid=aid
+	bucketName := form.Get("bucket")
+
+	//json.Unmarshal(bytes, intrfc)
+	// /&logSessData
+
+	insertQuery := insertQueryBuilder(bucketName, docID, string(bytes))
+	//insertQuery := insertQueryBuilder(bucketName, docID, intrfc)
+
+	//fmt.Println(insertQuery)
+	nqlInsertStatement := sqlStatementJSON(insertQuery)
+	//fmt.Println()
+	//fmt.Println(nqlInsertStatement)
+
+	responseMessage := db.queryRequest(nqlInsertStatement)
+
+	return responseMessage
+}
+
+//Upsert method for update and insert both
+func (db *DB) Upsert(form url.Values, intrfc interface{}) *ResponseMessage {
+
+	//bucketName := "master_erp"
+	//docID := "12121"
+	bytes := db.ProcessData(form, intrfc)
+	//db.ProcessData(form, intrfc)
+	//json.Unmarshal(bytes, intrfc) //***s
+
+	//fmt.Println("DATA>>>>", intrfc)
+
+	//bytes, _ := json.Marshal(intrfc)
+	//fmt.Println("intrfcBytes:", string(bytes))
+
+	docID := form.Get("aid") //docid=aid
+	bucketName := form.Get("bucket")
+
+	//json.Unmarshal(bytes, intrfc)
+	// /&logSessData
+
+	insertQuery := upsertQueryBuilder(bucketName, docID, string(bytes))
+	//insertQuery := insertQueryBuilder(bucketName, docID, intrfc)
+
+	//fmt.Println(insertQuery)
+	nqlInsertStatement := sqlStatementJSON(insertQuery)
+	//fmt.Println()
+	//fmt.Println(nqlInsertStatement)
+
+	responseMessage := db.queryRequest(nqlInsertStatement)
+
+	return responseMessage
+}
+
+func insertQueryBuilder(bucketName, docID, bytesStr string) (nqlStatement string) {
+
+	//docID := fmt.Sprintf("%s::%v", tableName, totalDocs)
+	//UPSERT
+	qs := `INSERT INTO %s (KEY, VALUE)
+	VALUES ("%s", %s)
+	RETURNING *`
+
+	nqlStatement = fmt.Sprintf(qs, bucketName, docID, bytesStr)
+
+	return
+
+}
+
+func upsertQueryBuilder(bucketName, docID, bytesStr string) (nqlStatement string) {
+
+	qs := `UPSERT INTO %s (KEY, VALUE)
+	VALUES ("%s", %s)
+	RETURNING *`
+	nqlStatement = fmt.Sprintf(qs, bucketName, docID, bytesStr)
+	return
+}
+
+//Struct fields can be accessed through a struct pointer.
+
+func prepareData(form url.Values, intrfc interface{}) *orderedmap.OrderedMap {
+
+	//uMap := make(map[string]interface{}, 0)
+	roMap := orderedmap.NewOrderedMap() //return ordered map
+
+	typeSlice := readSructColumnsType(intrfc)
+	oMap := keyValOrder(form, intrfc)
+
+	for i, key := range oMap.Keys() {
+		value, _ := oMap.Get(key)
+		vtype := typeSlice[i]
+		//fmt.Println(key, "==", value, "->", vtype)
+
+		var keyValue string = fmt.Sprintf("%v", value)
+
+		if vtype == "int" {
+
+			kValue, _ := strconv.Atoi(keyValue)
+			roMap.Set(key, kValue)
+			//valid := roMap.Set(key, kValue)
+			//fmt.Println(key, valid)
+
+		} else if vtype == "int64" {
+
+			kValue, _ := strconv.ParseInt(keyValue, 10, 64)
+			roMap.Set(key, kValue)
+
+		} else {
+
+			roMap.Set(key, value.(string))
+		}
+
+	}
+
+	return roMap
+}
+
+func readSructColumnsType(i interface{}) []string {
+
+	//cols := []string{}
+	cols := make([]string, 0)
+	iVal := reflect.ValueOf(i).Elem()
+	//typ := iVal.Type()
+	//fmt.Printf("typ: %v", typ)
+	for i := 0; i < iVal.NumField(); i++ {
+
+		f := iVal.Field(i)
+		// tag := typ.Field(i).Tag.Get("json")
+		// cols = append(cols, tag)
+
+		//f.Interface().(type)
+		vtype := f.Kind().String()
+		//fmt.Printf(", kind: %v", f.Kind().String())
+		cols = append(cols, vtype)
+
+	}
+
+	return cols
+}
+
+//KeyValOrder ...
+func keyValOrder(form url.Values, intrfc interface{}) *orderedmap.OrderedMap {
+
+	//uMap := make(map[string]interface{}, 0)
+	oMap := orderedmap.NewOrderedMap()
+	iVal := reflect.ValueOf(intrfc).Elem()
+	typ := iVal.Type()
+
+	for i := 0; i < iVal.NumField(); i++ {
+
+		tag := typ.Field(i).Tag.Get("json")
+
+		if strings.Contains(tag, ",") == true {
+			commaFoundAt := strings.Index(tag, ",")
+			//fmt.Println("commaFoundAt-->", commaFoundAt)
+			tag = tag[0:commaFoundAt]
+		}
+
+		//cols = append(cols, tag)
+		oMap.Set(tag, form.Get(tag))
+		//fmt.Println(">>", tag, "=", form.Get(tag))
+
+	}
+
+	return oMap
+}
+
+//
+func sqlStatementJSON(sql string) string {
+
+	nqlObj := new(nqlQuery)
+	nqlObj.Statement = sql //fmt.Sprintf(`SELECT * FROM master_erp WHERE type="%v"`, "login_session")
+	//nqlObj.Pretty = true
+	//nqlObj.Metrics = false
+
+	rbytes, _ := json.Marshal(nqlObj)
+	//fmt.Println(string(rbytes))
+
+	return string(rbytes)
+}
+
+//UPDATE master_erp SET field1=val1,field2=val2 ... WHERE condf1=condval1 AND ...;
